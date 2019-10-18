c331fbde84d73fc1e05accd1e78a08be6b6941f0
EC2 Default User <ec2-user@ip-172-31-38-192.us-east-2.compute.internal>
2019-10-18 11:02:12 +0000

    レビュー画面作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_090220) do
+ActiveRecord::Schema.define(version: 2019_10_18_095807) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_10_18_090220) do
     t.string "director"
     t.text "detail"
     t.string "open_date"
+  end
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname"
+    t.integer "rate"
+    t.text "review"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
